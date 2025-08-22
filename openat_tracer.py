d6b79e49bf197a03987f7fd0cb224767f5768057
Rohit Kumar <rohittourister@gmail.com>
2025-08-22 14:20:50 +0530

    Update openat_tracer.py

--- openat_tracer.py
+++ openat_tracer.py
@@ -4,24 +4,6 @@ bpf_code = """
 #include <uapi/linux/ptrace.h>
 #include <linux/sched.h>
 
-// Check if filename ends with ".env"
-static __inline int endswith_env(const char *fname) {
-    #pragma unroll
-    for (int i = 4; i < 256; i++) {
-        if (fname[i] == '\\0') {
-            if (fname[i-1] == 'v' &&
-                fname[i-2] == 'n' &&
-                fname[i-3] == 'e' &&
-                fname[i-4] == '.') {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-    }
-    return 0;
-}
-
 TRACEPOINT_PROBE(syscalls, sys_enter_openat) {
     char comm[16] = {};
     char fname[256] = {};
@@ -29,10 +11,8 @@ TRACEPOINT_PROBE(syscalls, sys_enter_openat) {
     bpf_get_current_comm(&comm, sizeof(comm));
     int ret = bpf_probe_read_user_str(&fname, sizeof(fname), args->filename);
 
-    if (ret > 0 && endswith_env(fname)) {
-        bpf_trace_printk("[.env READ] Process: %s\\n", comm);
-        bpf_trace_printk("             File: %s\\n", fname);
-    }
+    bpf_trace_printk("[.env READ] Process: %s\\n", comm);
+    bpf_trace_printk("             File: %s\\n", fname);
 
     return 0;
 }
@@ -44,10 +24,8 @@ TRACEPOINT_PROBE(syscalls, sys_enter_openat2) {
     bpf_get_current_comm(&comm, sizeof(comm));
     int ret = bpf_probe_read_user_str(&fname, sizeof(fname), args->filename);
 
-    if (ret > 0 && endswith_env(fname)) {
-        bpf_trace_printk("[.env READ] Process: %s\\n", comm);
-        bpf_trace_printk("             File: %s\\n", fname);
-    }
+    bpf_trace_printk("[.env READ] Process: %s\\n", comm);
+    bpf_trace_printk("             File: %s\\n", fname);
 
     return 0;
 }
